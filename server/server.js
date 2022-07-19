6eeae98d59dd4ae4124c31a979618ebb9947267c
IonicCraze <daviddeitz97@gmail.com>
2022-07-19 15:47:34 -0400

    deploy

--- server/server.js
+++ server/server.js
@@ -21,8 +21,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', function (req, res) {
+  res.sendFile('index.html');
 });
 
 
